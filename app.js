7db371dd1062da7985d9241d77beeca462cd0837
edytos <104804984+withplusplay@users.noreply.github.com>
2022-10-24 11:03:38 +0900

    Calculate Profit

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ document.addEventListener("DOMContentLoaded", () => {
   let isGameOver = false;
 
   const initialCapital = 500000000; // 초기자본 5억
-  const cost = 100000000; // 클릭당 비용 1억
+  const costExp = 100000000; // 클릭당 비용 지수 1억
+  const mulExp = 200000000; // 획득 골드당 배수 지수(multiple exponent)
   const goldAmount = 51; // 골드 총량
+  let totalCost = 0; // 총비용
+  let clickCount = 0; // 유효 클릭 카운트
   let revenue = 0; // 수익
   let profit = 0; // 이익
   let getGold = 0; // 획득 골드수
@@ -188,21 +191,38 @@ document.addEventListener("DOMContentLoaded", () => {
       square.innerHTML = "<img src='./img/bomb.png' >";
       square.classList.remove("bomb");
       square.classList.add("checked");
+      clickCount++;
     } else {
       square.innerHTML = "<img src='./img/goldbar.png' >";
       square.classList.remove("gold");
       square.classList.add("checked");
       getGold++;
-      foundGold.innerHTML = getGold;
+      clickCount++;
+      //foundGold.innerHTML = getGold;
     }
-    checkForWin();
+
+    totalCost = costExp * clickCount;
+    profit = calProfit() - totalCost;
+    foundGold.innerHTML = profit;
+    checkForComplete();
   }
 
-  //check for win
-  function checkForWin() {
+  // calculate profit
+  function calProfit() {
+    let mProfit = 0;
+
+    revenue = getGold * mulExp;
+    // 초기자본(5억) + 수익(황금획득갯수 X 2억) - 비용(1억/클릭) = 이익
+    // mProfit = initialCapital + getGold * mulExp - totalCost;
+    mProfit = initialCapital + revenue;
+    return mProfit;
+  }
+
+  //check for complate
+  function checkForComplete() {
     ///simplified win argument
     if (getGold === goldAmount) {
-      result.innerHTML = "YOU WIN!";
+      result.innerHTML = "Found All";
       isGameOver = true;
     }
   }
